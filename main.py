51ee9a47b7339f2611cd56a20d91cb0551ab9426
ReeseRuffin <Reese.Ruffin@Genesys.com>
2022-09-26 19:30:38 -0400

    This is my first Python Project

---
+++ main.py
@@ -0,0 +1,22 @@
+# print('The new player that will be joining this world is named, ' + input("And who is this new player?: "))
+
+# print('I need to lose more weight')
+
+# newPlayerName = input("What is your name: ")
+# print("The new user name is, " + newPlayerName)
+
+# print(len(input("What is your name? ")))
+
+# This is Day1 Project: Band Name Generator
+
+print("Hello, Welcome to Band Name Generator!")
+
+city = input("What city did you grow up in? \n")
+print("I see you grew up in " + city + "!")
+pet = input("What is your ugly pet name? \n")
+print("Ahhh, I see your pet name is " + pet)
+bandName = city + pet
+print("Your band name is, " + bandName)
+
+
+print("Thank you for testing the Band Name Generator and have a good day!")
